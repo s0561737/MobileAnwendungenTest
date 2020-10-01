f6a81a0bfdfb07f50953f113b82f9fd4313fa7d9
s0561737 <nadin.dressler@student.htw-berlin.de>
2020-10-01 13:31:39 +0200

    testUpdate myFirstStep

--- stepOne/src/stepOne/myFirstStep.java
+++ stepOne/src/stepOne/myFirstStep.java
@@ -4,7 +4,7 @@ public class myFirstStep {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, mobile Anwendungen WI20/21!");
+		System.out.println("Hello, mobile Anwendungen WiSe20/21!");
 	}
 
 }
